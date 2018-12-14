ffa8e1e2d9d7d05c8b79e4893dc0d2ad392c8c40
Sheldon Siao <sheldonsiao@bluetechnology.com.tw>
2018-12-14 21:54:37 +0800

    referendum init

---
+++ src/main/java/com/sheldon/referendum/ReferendumApplication.java
@@ -0,0 +1,14 @@
+package com.sheldon.referendum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferendumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferendumApplication.class, args);
+	}
+
+}
+
